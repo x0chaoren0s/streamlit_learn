b1595dbb857820cbd9ae9e6b76b40dca556a6640
x0chaoren0s <x0chaoren0s@gmail.com>
2022-12-21 13:43:43 +0800

    入口更新

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 
-import day3 as app
+import streamlit as st
+import os, importlib
 
-import importlib
+st.sidebar.info('出现 DuplicateWidgetID 报错后，按 [R] 或点击右上角设置 Rerun')
+
+challenge_list = [file.split('.py')[0] for file in os.listdir() 
+                if file.startswith('day')]
+
+challenge_day = st.sidebar.selectbox('Challenge', challenge_list)
+
+app = importlib.import_module(challenge_day)
 importlib.reload(app)
